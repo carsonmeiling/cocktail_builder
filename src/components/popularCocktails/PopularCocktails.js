da6afb368a02f03301e1c813cdee1cd9a978e75e
Carson Meiling <carmc93@gmail.com>
2020-11-19 21:04:08 -0700

    changed api call to axios, now grabs data

--- src/components/popularCocktails/PopularCocktails.js
+++ src/components/popularCocktails/PopularCocktails.js
@@ -1,25 +1,23 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 
 const PopularCocktails  = () => {
 
   const[cocktails, setCocktails] = useState({});
 
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a", {
-    "method": "GET",
-    // "headers": {
-    //   "x-rapidapi-key": "0a157b1b2emsh1de672134471601p14a7e7jsn8ee1f917a58b",
-    //   "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
-    // }
+  axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php", {
+    
   })
-  .then(response => {
-    console.log(response);
-    setCocktails(response)
+  .then(res => {
+    console.log(res);
+    setCocktails(res)
+    debugger
   })
   .catch(err => {
     console.error(err);
   });
-
+    
   return(
     <div>
       <p>Future drink list</p>
